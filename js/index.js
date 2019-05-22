7566c3fec463e84c7a9f8262500378c4b4ff1da3
Akin <akinpds@gmail.com>
2019-05-22 14:34:08 -0500

    Rendered a life count for halle bot and gameover alert once 3 lives are over

--- js/index.js
+++ js/index.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
         game.score = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
     }
 
+
     function update() {
         const asset = game.player.asset,
               playerManager = game.playerManager,
@@ -37,11 +38,30 @@ $(document).ready(function () {
         playerManager.update();
     }
     
+    let lives = 3;
+
     function onProjectileOverlap() {
         console.log('Halle hit!');
         game.player.die();
+        decrementLives();
+        let lives = $("b").map(function () {
+            return $(this).text();
+        }).get();
+        if(lives > 0){
+            opspark.player.init(game);
+        } 
     }
-    
+    function decrementLives(){
+        if(lives !== 0){
+            lives--;
+            $('.lives').text(lives)
+        } else if(lives === 0){
+            alert('GameOver! Please, refresh your browser to try again.');
+        } 
+    }
+
+    $('<b>').text(lives).attr('class', 'lives').appendTo($('body'))
+
     function collectDb(player, collectable) {
         game.score.text = 'Score: ' + (parseInt(/\s+(\S*)$/.exec(game.score.text)[1], 10) + collectable.type.points);
         collectable.kill();
